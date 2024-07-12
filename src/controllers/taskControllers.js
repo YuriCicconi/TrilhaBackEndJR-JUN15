c4022399cb22e89fccb5453f322114c6457e2d3f
Yuri Cicconi <yuricbianchii@gmail.com>
2024-07-12 16:48:13 -0300

    feat: created a task controller that create a new task and define the user_id directly from the token owner

---
+++ src/controllers/taskControllers.js
@@ -0,0 +1,22 @@
+const knex = require('../database/connection');
+
+async function createTask(req, res) {
+    const { name, description } = req.body;
+    const user_id = req.userId;
+
+    if (!name) return res.status(400).json({ message: 'Name is a required field.' });
+
+    try {
+        const task = { user_id, name, description };
+
+        const newTask = await knex('tasks').insert(task).returning('*');
+
+        return res.status(201).json(newTask);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = {
+    createTask
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 
 const express = require('express');
 const users = require('../controllers/usersControllers');
+const tasks = require('../controllers/taskControllers');
 const verifyUser = require('../middlewares/verifyUser');
 const routes = express();
 
 routes.post('/api/login', users.login);
-routes.use(verifyUser);
 routes.post('/api/users', users.createUser);
+
+routes.use(verifyUser);
+
 routes.get('/api/users', users.returnUsers);
+
+routes.post('/api/tasks', tasks.createTask);
 
 module.exports = routes;
