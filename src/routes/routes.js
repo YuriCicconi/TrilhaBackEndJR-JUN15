e6a6886d46e861c83257d965f9036755c0e18a76
Yuri Cicconi <yuricbianchii@gmail.com>
2024-07-16 17:18:23 -0300

    added a test route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,9 @@ const tasks = require('../controllers/taskControllers');
 const verifyUser = require('../middlewares/verifyUser');
 const routes = express();
 
+routes.get('/', (req, res) => {
+    return res.json({ message: "Funcionou" })
+})
 routes.post('/api/login', users.login);
 routes.post('/api/users', users.createUser);
 
